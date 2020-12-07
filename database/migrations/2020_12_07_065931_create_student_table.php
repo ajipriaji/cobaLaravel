3a5c126169cc820038f6394b2e3461426b8a85c8
priaji <ajipriaji10@student.uns.ac.id>
2020-12-07 22:57:28 +0700

    Initial Commit

---
+++ database/migrations/2020_12_07_065931_create_student_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
+
+class CreateStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama',255);
+            $table->string('nim',10)->unique();
+            $table->string('email',255)->unique();
+            $table->string('jurusan',255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+}
